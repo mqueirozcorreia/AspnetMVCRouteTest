c14e797a7717db150386acb698eaba41ce3c64ac
Mateus Queiroz Correia <mateus_queiroz@hotmail.com>
2018-03-29 12:50:06 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Test for the question and answers of (Asp.net MVC 5 Attribute Routing Constraint)[https://stackoverflow.com/questions/49523867/asp-net-mvc-5-attribute-routing-constraint/49523891?noredirect=1#comment86065404_49523891]
+
+Tests using Attribute:
+http://localhost:50964/home/edit = OK
+http://localhost:50964/home/edit?promoid=5 = OK
+http://localhost:50964/home/edit?promoid=a = Error 500
+
+
+Tests using MapRoute
+http://localhost:50964/home/EditFromMapRoute = OK
+http://localhost:50964/home/EditFromMapRoute?promoid=asd = OK
+http://localhost:50964/home/EditFromMapRoute?promoid=5 = OK
---
+++ RouteTest/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RouteTest.Controllers
+{
+    [RoutePrefix("Home")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("edit")]
+        public ActionResult Edit(int promoId)
+        {
+            return Content("FromUri OK");
+        }
+
+        public ActionResult EditFromMapRoute(int? promoId)
+        {
+            return Content("EditFromMapRoute OK");
+        }
+    }
+}
